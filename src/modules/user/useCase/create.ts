d79d3b2ef58f3756b6a30c17a317eb9a8c504d0a
Cadu <carloseduardodiasm@gmail.com>
2023-02-27 13:29:06 -0300

    setting the event name

--- src/modules/user/useCase/create.ts
+++ src/modules/user/useCase/create.ts
@@ -2,11 +2,14 @@ import User, { IUser } from '../domain'
 import mixpanel from '../../../tools/mixpanel'
 
 function integrateOnMixpanelEngage (user: IUser) {
-  return mixpanel.track('event name', user)
+  const userCopy = { ...user }
+  const eventName = 'Criação de usuário'
+  mixpanel.track(eventName, userCopy)
+  return { ...userCopy, eventName }
 }
 
 export default function (name: string, email: string): IUser {
   const newCreatedUser = new User({ name, email })
-  integrateOnMixpanelEngage(newCreatedUser)
+  console.log(integrateOnMixpanelEngage(newCreatedUser))
   return newCreatedUser
 }
